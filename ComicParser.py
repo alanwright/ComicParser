c1b54827579acbc6c643447c16ca647af6d0b1dd
Alan Wright <alanwright47@gmail.com>
2014-10-26 23:32:33 -0700

    hasImage flag, and JSON fix

--- ComicParser.py
+++ ComicParser.py
@@ -9,20 +9,23 @@ import json
 
 #Constants
 DOMAIN = 'http://leagueofcomicgeeks.com/'
+NEW_COMICS_DOMAIN = DOMAIN + 'comics/new-comics'
 DETAIL_SPLIT = '&nbsp;&#183;&nbsp;'
+NO_COVER = 'no-cover'
 
 #Comic class to store all our details
 class Comic:
-	def __init__(self, title = None, publisher = None, releaseDate = None, price = None, thumbnail = None, image = None):
+	def __init__(self, title = None, publisher = None, releaseDate = None, price = None, thumbnail = None, image = None, hasImage = False):
 		self.title = title
 		self.publisher = publisher
 		self.releaseDate = releaseDate
 		self.price = price
 		self.thumbnail = thumbnail
 		self.image = image
+		self.hasImage = hasImage
 
 	def __str__(self):
-		return "Title: %s\nPublisher: %s\nRelease Date: %s\nPrice: %s\nThumbnail: %s\nImage: %s" % (self.title, self.publisher, self.releaseDate, self.price, self.thumbnail, self.image) 
+		return "Title: %s\nPublisher: %s\nRelease Date: %s\nPrice: %s\nHas Image: %s\nThumbnail: %s\nImage: %s" % (self.title, self.publisher, self.releaseDate, self.price, self.hasImage, self.thumbnail, self.image) 
 
 	#Makes a nice json format for our object
 	def to_JSON(self):
@@ -30,7 +33,7 @@ class Comic:
 
 
 #Fetch page and releases
-html = urllib2.urlopen('http://leagueofcomicgeeks.com/comics/new-comics')
+html = urllib2.urlopen(NEW_COMICS_DOMAIN)
 homepage_parsed_html = BeautifulSoup(html)
 entries = homepage_parsed_html.body.findAll('li', attrs={'class':'media'})
 
@@ -38,7 +41,7 @@ entries = homepage_parsed_html.body.findAll('li', attrs={'class':'media'})
 print 'Found', len(entries), 'new release entries'
 
 comics = []
-comics_JSON = '{"comics":[' #this will store our json string to output
+comics_JSON = '[' #this will store our json string to output
 for entry in entries:
 	comic = Comic()
 
@@ -55,6 +58,7 @@ for entry in entries:
 	#Parse images
 	comic.thumbnail = DOMAIN + entry.find('img').get('src')
 	comic.image = comic.thumbnail.replace("medium", "large")
+	comic.hasImage = NO_COVER not in comic.thumbnail
 
 	#Print and save comic json
 	print comic, '\n'
@@ -62,6 +66,6 @@ for entry in entries:
 	comics_JSON = comics_JSON + str(comic.to_JSON()) + ','
 
 #Write out json to file
-comics_JSON = comics_JSON[0 : len(comics_JSON) - 2] + ']}' #trim extra ','
+comics_JSON = comics_JSON[0 : len(comics_JSON) - 2] + ']' #trim extra ','
 with open('comics.json', 'w') as outfile:
 	outfile.write(comics_JSON)	
